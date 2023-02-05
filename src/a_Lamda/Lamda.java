ac774b4588cd091f2096c121f067d71964ebf099
parktaeyang <pty1026@naver.com>
2023-02-05 14:42:31 +0900

    Lamda 생성 및 호출.

--- src/a_Lamda/Lamda.java
+++ src/a_Lamda/Lamda.java
@@ -1,5 +1,11 @@
 package a_Lamda;
 
 public class Lamda {
-
+    @FunctionalInterface
+    public interface Lamda_cal {
+        public int Calc(int first, int second);
+    }
+    
+    Lamda_cal L_Calc = (f, s) -> f+s;
+    
 }
